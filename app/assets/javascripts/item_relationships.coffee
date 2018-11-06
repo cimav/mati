6df777b72cfadc8ccb1cceeb6e4fa807f4b05ea5
Jonathan Hernández <jonathan.hernandez@gmail.com>
2018-11-05 17:06:55 -0700

    Relaciones WIP

--- app/assets/javascripts/item_relationships.coffee
+++ app/assets/javascripts/item_relationships.coffee
@@ -1,3 +1,15 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on('change', '.rel-radio', () ->
+  $('#item-relationship-name').html($('#label-' + this.value).html().toLowerCase())
+)
+
+$(document).on('change', '.rel-cb-item', () ->
+  selected = [];
+  $('.rel-cb-item:checked').each () ->
+    selected.push($(this).data('name'))
+
+  $('#item-relationship-to').html(selected.join(', '))
+)
--- app/controllers/item_relationships_controller.rb
+++ app/controllers/item_relationships_controller.rb
@@ -1,6 +1,29 @@
 class ItemRelationshipsController < ApplicationController
-  before_action :set_item, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_item, only: [:index, :show, :new, :edit, :update, :destroy, :create]
   before_action :set_item_relationship, only: [:show, :edit, :update, :destroy]
+
+
+  def new
+    @item_relationship = ItemRelationship.new
+  end
+
+  def create
+    respond_to do |format|
+      params[:item].each do |i|
+        rel_params = params[:relationship].split('-')
+        if rel_params[0] == 'dir'
+          rel = @item.relationship_children.new
+          rel.child_id  = i
+        else
+          rel = @item.relationship_parents.new
+          rel.parent_id  = i
+        end
+        rel.relationship_id = rel_params[1]
+        rel.save
+      end
+      format.html { redirect_to item_item_relationships_path(@item), notice: 'Relación creada.' }
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -8,6 +31,6 @@ class ItemRelationshipsController < ApplicationController
       @item = Item.find(params[:item_id])
     end
     def set_item_relationship
-      @item = Item.find(params[:item_id])
+      @item = ItemRelationship.find(params[:id])
     end
 end
